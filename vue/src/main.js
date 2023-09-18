417ce4d3edaa10354fca398de34ce898fe2e92a9
Gabriel Kryger <gabrielkrygermesa@gmail.com>
2023-09-18 00:21:26 -0300

    cambios de endpoints

--- vue/src/main.js
+++ vue/src/main.js
@@ -12,8 +12,8 @@ import Login from './views/Login.vue';
 import Register from './views/Register.vue';
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/Login', component: Login },
+  { path: '/Home', component: Home },
+  { path: '/', component: Login },
   { path: '/Register', component: Register }
 ];
 
